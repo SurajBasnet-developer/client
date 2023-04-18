71e69b46357e8362725ec026c6c62d5e6d1d102a
Suraj <surajbasnet449@gmail.com>
2023-04-18 17:38:30 +0545

    minor

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./components/Home";
 import ChatPage from "./components/ChatPage";
 import socketIO from "socket.io-client";
 
-const socket = socketIO.connect("https://cha-ten.vercel.app");
+const socket = socketIO.connect("https://chsok.onrender.com");
 function App() {
   return (
     <BrowserRouter>
